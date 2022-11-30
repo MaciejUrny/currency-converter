4c48e4c447a8187dbe49a16bff51442ee06e5391
Maciej Urny <maciej.urny@o2.pl>
2022-11-30 22:42:37 +0100

    Change let to const

--- js/script.js
+++ js/script.js
@@ -3,21 +3,21 @@
         console.log("Witaj, miło Cię tu widzieć.");
     };
     
-    let formElement = document.querySelector(".js-form");
-    let amountElement = document.querySelector(".js-amountPLN");
-    let currencySelectionElement = document.querySelector(".js-currencySelection");
-    let resultElement = document.querySelector(".js-result");
+    const formElement = document.querySelector(".js-form");
+    const amountElement = document.querySelector(".js-amountPLN");
+    const currencySelectionElement = document.querySelector(".js-currencySelection");
+    const resultElement = document.querySelector(".js-result");
 
-    let rateEUR = 4.6898;
-    let rateCHF = 4.7523;
-    let rateGBP = 5.3825;
-    let rateUSD = 4.7975;
+    const rateEUR = 4.6898;
+    const rateCHF = 4.7523;
+    const rateGBP = 5.3825;
+    const rateUSD = 4.7975;
 
     formElement.addEventListener("submit", (event) => {
         event.preventDefault();
 
-        let amount = +amountElement.value;
-        let currency = currencySelectionElement.value;
+        const amount = +amountElement.value;
+        const currency = currencySelectionElement.value;
 
         let result;
         let currencyValue;
