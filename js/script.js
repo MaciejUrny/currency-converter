5f2b5cb24d7817150693001e7ac5dba43fe8cf6a
Maciej Urny <maciej.urny@o2.pl>
2022-12-02 17:56:20 +0100

    function catchCurrencyValue added

--- js/script.js
+++ js/script.js
@@ -3,16 +3,32 @@
         console.log("Witaj, miło Cię tu widzieć.");
     };
 
+    const catchCurrencyValue = (currency) => {
+        const rateEUR = 4.6898;
+        const rateCHF = 4.7523;
+        const rateGBP = 5.3825;
+        const rateUSD = 4.7975;
+
+        switch (currency) {
+            case "EUR":
+                return rateEUR;
+
+            case "CHF":
+                return rateCHF;
+
+            case "GBP":
+                return rateGBP;
+
+            case "USD":
+                return rateUSD;
+        };
+    };
+
     const init = () => {
         const formElement = document.querySelector(".js-form");
         const amountElement = document.querySelector(".js-amountPLN");
         const currencySelectionElement = document.querySelector(".js-currencySelection");
         const resultElement = document.querySelector(".js-result");
-
-        const rateEUR = 4.6898;
-        const rateCHF = 4.7523;
-        const rateGBP = 5.3825;
-        const rateUSD = 4.7975;
 
         formElement.addEventListener("submit", (event) => {
             event.preventDefault();
@@ -21,26 +37,7 @@
             const currency = currencySelectionElement.value;
 
             let result;
-            let currencyValue;
-
-            switch (currency) {
-                case "EUR":
-                    result = amount / rateEUR;
-                    currencyValue = rateEUR;
-                    break;
-                case "CHF":
-                    result = amount / rateCHF;
-                    currencyValue = rateCHF;
-                    break;
-                case "GBP":
-                    result = amount / rateGBP;
-                    currencyValue = rateGBP;
-                    break;
-                case "USD":
-                    result = amount / rateUSD;
-                    currencyValue = rateUSD;
-                    break;
-            };
+            const currencyValue = catchCurrencyValue(currency);
 
             resultElement.value = `${amount} PLN / ${currencyValue.toFixed(2)} ${currency} = ${result.toFixed(2)} ${currency}`;
         });
