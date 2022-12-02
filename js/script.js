958ca4da8488eb46567145771c499dc931555113
Maciej Urny <maciej.urny@o2.pl>
2022-12-02 17:44:58 +0100

    Function init added

--- js/script.js
+++ js/script.js
@@ -1,48 +1,54 @@
 {
-    const welcome = () =>{
+    const welcome = () => {
         console.log("Witaj, miło Cię tu widzieć.");
     };
-    
-    const formElement = document.querySelector(".js-form");
-    const amountElement = document.querySelector(".js-amountPLN");
-    const currencySelectionElement = document.querySelector(".js-currencySelection");
-    const resultElement = document.querySelector(".js-result");
 
-    const rateEUR = 4.6898;
-    const rateCHF = 4.7523;
-    const rateGBP = 5.3825;
-    const rateUSD = 4.7975;
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+        const amountElement = document.querySelector(".js-amountPLN");
+        const currencySelectionElement = document.querySelector(".js-currencySelection");
+        const resultElement = document.querySelector(".js-result");
 
-    formElement.addEventListener("submit", (event) => {
-        event.preventDefault();
+        const rateEUR = 4.6898;
+        const rateCHF = 4.7523;
+        const rateGBP = 5.3825;
+        const rateUSD = 4.7975;
 
-        const amount = +amountElement.value;
-        const currency = currencySelectionElement.value;
+        formElement.addEventListener("submit", (event) => {
+            event.preventDefault();
 
-        let result;
-        let currencyValue;
+            const amount = +amountElement.value;
+            const currency = currencySelectionElement.value;
 
-        switch (currency) {
-            case "EUR":
-                result = amount / rateEUR;
-                currencyValue = rateEUR;
-                break;
-            case "CHF":
-                result = amount / rateCHF;
-                currencyValue = rateCHF;
-                break;
-            case "GBP":
-                result = amount / rateGBP;
-                currencyValue = rateGBP;
-                break;
-            case "USD":
-                result = amount / rateUSD;
-                currencyValue = rateUSD;
-                break;
-        };
+            let result;
+            let currencyValue;
 
-        resultElement.value = `${amount} PLN / ${currencyValue.toFixed(2)} ${currency} = ${result.toFixed(2)} ${currency}`;
-    });
+            switch (currency) {
+                case "EUR":
+                    result = amount / rateEUR;
+                    currencyValue = rateEUR;
+                    break;
+                case "CHF":
+                    result = amount / rateCHF;
+                    currencyValue = rateCHF;
+                    break;
+                case "GBP":
+                    result = amount / rateGBP;
+                    currencyValue = rateGBP;
+                    break;
+                case "USD":
+                    result = amount / rateUSD;
+                    currencyValue = rateUSD;
+                    break;
+            };
 
-    welcome();
+            resultElement.value = `${amount} PLN / ${currencyValue.toFixed(2)} ${currency} = ${result.toFixed(2)} ${currency}`;
+        });
+
+        welcome();
+
+    };
+
+    init();
+
 }
